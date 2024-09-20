280945bbdf969eaba7abc531c8b6cd88ffded366
Matheus <mmatheus_silvam@outlook.com>
2024-09-19 22:07:48 -0300

    Criação da Operação 'Soma (+)'

--- App.js
+++ App.js
@@ -1,20 +1,67 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TouchableHighlight, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [valor1, setValor1] = useState(10);
+  const [valor2, setValor2] = useState(10);
+  const [resultado, setResultado] = useState(20);
+
+  const soma = () => {
+    const num1 = parseFloat(valor1);
+    const num2 = parseFloat(valor2);
+    setResultado(num1 + num2);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text>Calculadora</Text>
+
+      <TextInput
+        style={styles.display}
+        value={String(valor1)}
+        onChangeText={(text) => setValor1(text)}
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.display}
+        value={String(valor2)}
+        onChangeText={(text) => setValor2(text)}
+        keyboardType="numeric"
+      />
+
+      <TextInput
+        style={styles.display}
+        value={String(resultado)}
+        editable={false}
+      />
+
+      <TouchableHighlight onPress={soma}>
+        <View>
+          <Text style={styles.btn}>Somar</Text>
+        </View>
+      </TouchableHighlight>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+  },
+  display: {
+    borderWidth: 1,
+    borderRadius: 10,
+    padding: 10,
+    marginVertical: 10,
+  },
+  btn: {
+    backgroundColor: '#aaa',
+    padding: 20,
+    textAlign: 'center',
   },
 });
